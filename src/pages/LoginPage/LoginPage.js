553ba6c8bfa396f71e12173d487a56142aaee360
yurii-kovalchuk <sogdrums@gmail.com>
2023-03-02 18:51:18 +0200

    add login-page init markup

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,3 +1,27 @@
 export const LoginPage = () => {
-  return <h2>login page</h2>;
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.dir({
+      email: e.currentTarget.elements.email.value,
+      password: e.currentTarget.elements.password.value,
+    });
+    e.currentTarget.reset();
+  };
+  return (
+    <div>
+      <h2>login page</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 };
